a5af463134d83d26e2ae2de5fd20c6ce7b3dda8f
Meder <m.toktosunov@icloud.com>
2018-11-28 23:23:50 -0500

    refactored main.rb

---
+++ main.rb
@@ -0,0 +1,43 @@
+require 'pry'
+require_relative './guessing_game.rb'
+
+
+score = 0
+
+puts "Welcome to the Guessing Game!"
+print "What difficulty would you like to play? "
+
+while true
+  puts "Type easy or hard:"
+  print "> "
+  difficulty = gets.chomp.downcase
+
+  game = Game.new(difficulty)
+
+  case difficulty
+  when 'easy'
+    level = 10
+    break
+  when "hard"
+    level = 10
+    break
+  else
+    puts "Invalid input, please enter 'hard' or 'easy'"
+  end
+end
+
+puts "I've picked a number between 1 and #{level}. Now guess it!"
+
+while true
+  score += 1
+  print "> "
+  guess = gets.chomp.to_i
+  if game.check_guess(guess)
+    puts "You win! Nice job!"
+    break
+  else
+    puts "Nope, try again!"
+  end
+end
+
+puts "Your score was: #{score}"
